54b722d0958881a27539af824212cfd0b869c4ed
Henrique AF <henriqueaf22@gmail.com>
2026-09-16 11:46:18 -0300

    Adicionar arquivos de projeto.

---
+++ Simplex-Engine/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Simplex_Engine.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
